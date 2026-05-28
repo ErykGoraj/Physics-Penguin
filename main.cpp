57eafc57bdbe7e5a1d790f667f07091d0bb105d4
Super User <170825619+Erandvil@users.noreply.github.com>
2026-05-28 11:19:55 +0200

    Utworzenie podstawowego szablonu projektu

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
